d7aa646a3e25a7e67904647cc0832d2d6d557846
Sandboxgamer <patrick@bronkemail.de>
2023-07-07 22:01:23 +0200

    Upload to GitHub

---
+++ src/de/sbg/unity/antimounttheft/AntiMountTheft.java
@@ -0,0 +1,19 @@
+package de.sbg.unity.antimounttheft;
+
+import net.risingworld.api.Plugin;
+
+
+public class AntiMountTheft extends Plugin{
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+    
+    
+}
